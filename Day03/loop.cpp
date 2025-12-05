5bcb71b20b1382833487712951e3c22e17d30fd5
Priyangshu Saha <priyangshu.saha.kolkata@gmail.com>
2025-12-05 16:59:48 +0530

    Add Day03: Loop and Pattern programs

---
+++ Day03/loop.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    // Even Number
+
+    // for(int i=2;i<=20;i=i+2){
+    //     cout<<i<<endl;
+    // };
+
+    // for(int i=1;i<=20;i++){
+    //     if(i%2==0){
+    //         cout<<i<<endl;
+    //     }
+    // }
+
+    // Odd Number
+
+    // for(int i=1;i<=20;i=i+2){
+    //    cout<<i<<endl;
+    // }
+
+    
+    // a to z
+
+    // for(char i='a';i<='z';i++){
+    //     cout<<i<<endl;
+    // }
+    
+// -113115040
+    // sum of first 10 number
+    
+    // int sum = 0;
+
+    // // cout<<sum;
+
+    // for(int i=1;i<=10;i++){
+    //     sum+=i;
+    // } 
+
+
+    // cout<<sum;
+
+    // sum of square of first 10 natural number
+
+    // int sum = 0;
+
+    // for(int i=1;i<=10;i++){
+    //     sum = sum+i*i;
+    // }
+
+    // cout<<sum;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+   
+
+
+    return 0;
+
+}
